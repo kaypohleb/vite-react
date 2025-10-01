0737a2ac239173af080cb60fc2f28c0be10efb7b
Caleb Foo <39551563+kaypohleb@users.noreply.github.com>
2025-10-01 15:28:36 +0800

    created background component

---
+++ src/components/BlendMoveBackground.tsx
@@ -0,0 +1,17 @@
+
+export default function BlendMoveBackground() {
+
+  return (
+    <div className="w-[100vw] h-[100dvh] bg-black overflow-hidden absolute top-0 left-0 z-0">
+      <div className="relative w-full h-full">
+        <div className="rainbow"></div>
+        <div className="rainbow"></div>
+        <div className="rainbow"></div> 
+        <div className="rainbow"></div>
+        <div className="rainbow"></div>
+        <div className="rainbow"></div>
+        <div className="rainbow"></div>
+      </div>
+    </div>
+  );
+}
